c8ac85241439eea42ce60f5b12638909b12976a3
HSM <hsadeghmoghaddasi@gmail.com>
2025-09-07 11:39:17 +0330

    wrapper added

--- FPCCSDK/Package.swift
+++ FPCCSDK/Package.swift
@@ -1,33 +1,35 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "FPCCSDK",
     platforms: [
-        .iOS(.v16) // iOS version target
+        .iOS(.v16)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        
         .library(
             name: "FPCCSDK",
-            targets: ["CreateCustomerSDK"]),
+            targets: ["FPCCSDKWrapper"]
+        ),
     ],
     dependencies: [
+        
         .package(url: "https://github.com/HSMDeveloper/FPKYCSDK.git", branch: "main")
     ],
     targets: [
-           .binaryTarget(
-               name: "CreateCustomerSDK",
-               path: "./Sources/FPCC.xcframework"
-           ),
-           .target(
-               name: "FPCCSDK",
-               dependencies: [
-                   "CreateCustomerSDK",
-                   .product(name: "FPKYCSDK", package: "FPKYCSDK")
-               ]
-           )
-       ]
+        
+        .binaryTarget(
+            name: "CreateCustomerSDK",
+            path: "./Sources/FPCC.xcframework"
+        ),
+        
+        .target(
+            name: "FPCCSDKWrapper",
+            dependencies: [
+                "CreateCustomerSDK",
+                .product(name: "FPKYCSDK", package: "FPKYCSDK")
+            ]
+        )
+    ]
 )
---
+++ FPCCSDK/Sources/FPCCSDKWrapper/FPCCSDKWrapper.swift
@@ -0,0 +1,10 @@
+//
+//  FPCCSDKWrapper.swift
+//  FPCCSDK
+//
+//  Created by Hassan Sadegh Moghaddasi on 9/7/25.
+//
+
+
+@_exported import CreateCustomerSDK
+@_exported import FPKYCSDK
